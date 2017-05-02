527ab847fc43090157bdaab216ea86f73a9bb0f0
CJT2325 <445263848@qq.com>
2017-05-02 16:11:10 +0800

    update

--- app/src/main/java/com/cjt2325/cameraview/MainActivity.java
+++ app/src/main/java/com/cjt2325/cameraview/MainActivity.java
@@ -29,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         jCameraView = (JCameraView) findViewById(R.id.jcameraview);
         /**
+         * 关闭声音
+         */
+        jCameraView.forbiddenAudio(true);
+        /**
          * 设置视频保存路径
          */
         jCameraView.setSaveVideoPath(Environment.getExternalStorageDirectory().getPath() + File.separator + "JCamera");
